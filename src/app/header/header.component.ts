71297239cec3c0f74199fa5f88dd79a99fd5889d
Mschoot1 <marnix_vd_schoot@hotmail.com>
2017-12-14 19:10:43 +0100

    users component added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthService} from '../auth/auth.service';
+import {Component} from '@angular/core';
 import {Router} from '@angular/router';
-import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {
-  }
-
-  ngOnInit() {
+export class HeaderComponent {
+  constructor(private router: Router) {
   }
 
   onLogout() {
