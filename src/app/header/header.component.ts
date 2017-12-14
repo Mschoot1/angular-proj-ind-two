76b0021c2f536950b5bd5d87659d45784e4b0b28
Mschoot1 <marnix_vd_schoot@hotmail.com>
2017-12-14 19:20:20 +0100

    commit to heroku

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
+import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  constructor(private router: Router) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   onLogout() {
