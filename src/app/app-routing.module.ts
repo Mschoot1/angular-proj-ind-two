e5d499752eda524008ec0b214fc5c7ab49ec93ff
Mschoot1 <marnix_vd_schoot@hotmail.com>
2017-12-14 19:47:36 +0100

    users component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,39 +4,39 @@ import {Routes, RouterModule} from '@angular/router';
 import {RegisterComponent} from './auth/register/register.component';
 import {LoginComponent} from './auth/login/login.component';
 import {AuthGuard} from './auth/auth-guard.service';
-// import {SchedulesComponent} from './schedules/schedules.component';
-// import {ScheduleEditComponent} from './schedules/schedule-edit/schedule-edit.component';
-// import {ScheduleDetailComponent} from './schedules/schedule-detail/schedule-detail.component';
-// import {MoviesComponent} from './movies/movies.component';
-// import {MovieEditComponent} from './movies/movie-edit/movie-edit.component';
-// import {MovieDetailComponent} from './movies/movie-detail/movie-detail.component';
-// import {ReservationsComponent} from './reservations/reservations.component';
-// import {ReservationEditComponent} from './reservations/reservation-edit/reservation-edit.component';
-// import {ReservationDetailComponent} from './reservations/reservation-detail/reservation-detail.component';
+import {SchedulesComponent} from './schedules/schedules.component';
+import {ScheduleEditComponent} from './schedules/schedule-edit/schedule-edit.component';
+import {ScheduleDetailComponent} from './schedules/schedule-detail/schedule-detail.component';
+import {MoviesComponent} from './movies/movies.component';
+import {MovieEditComponent} from './movies/movie-edit/movie-edit.component';
+import {MovieDetailComponent} from './movies/movie-detail/movie-detail.component';
+import {ReservationsComponent} from './reservations/reservations.component';
+import {ReservationEditComponent} from './reservations/reservation-edit/reservation-edit.component';
+import {ReservationDetailComponent} from './reservations/reservation-detail/reservation-detail.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/movies', pathMatch: 'full'},
-  // {
-  //   path: 'movies', component: MoviesComponent, children: [
-  //     {path: 'new', component: MovieEditComponent},
-  //     {path: ':title', component: MovieDetailComponent},
-  //     {path: ':title/edit', component: MovieEditComponent}
-  //   ], canActivate: [AuthGuard]
-  // },
-  // {
-  //   path: 'schedules', component: SchedulesComponent, children: [
-  //     {path: 'new', component: ScheduleEditComponent},
-  //     {path: ':_id', component: ScheduleDetailComponent},
-  //     {path: ':_id/edit', component: ScheduleEditComponent}
-  //   ], canActivate: [AuthGuard]
-  // },
-  // {
-  //   path: 'reservations', component: ReservationsComponent, children: [
-  //     {path: 'new', component: ReservationEditComponent},
-  //     {path: ':_id', component: ReservationDetailComponent},
-  //     {path: ':_id/edit', component: ReservationEditComponent}
-  //   ], canActivate: [AuthGuard]
-  // },
+  {
+    path: 'movies', component: MoviesComponent, children: [
+      {path: 'new', component: MovieEditComponent},
+      {path: ':title', component: MovieDetailComponent},
+      {path: ':title/edit', component: MovieEditComponent}
+    ], canActivate: [AuthGuard]
+  },
+  {
+    path: 'schedules', component: SchedulesComponent, children: [
+      {path: 'new', component: ScheduleEditComponent},
+      {path: ':_id', component: ScheduleDetailComponent},
+      {path: ':_id/edit', component: ScheduleEditComponent}
+    ], canActivate: [AuthGuard]
+  },
+  {
+    path: 'reservations', component: ReservationsComponent, children: [
+      {path: 'new', component: ReservationEditComponent},
+      {path: ':_id', component: ReservationDetailComponent},
+      {path: ':_id/edit', component: ReservationEditComponent}
+    ], canActivate: [AuthGuard]
+  },
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent}
 ];
